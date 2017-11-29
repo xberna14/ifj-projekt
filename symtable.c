0e5a57e5b4d97a5896e2c2ffa162437c08430a26
xberna14 <radkob12@gmail.com>
2017-11-29 20:11:53 +0100

    symtable

--- symtable.c
+++ symtable.c
@@ -13,7 +13,45 @@
 #include "lexer.h"
 
 void symTableInit(symtab_elem_t *Root){
-    Root = NULL;
+    *Root = NULL;
 }
 
-int symTableSearch(symtab_elem_t Root)
+int symTableSearch(symtab_elem_t Root, elem_type typ, string nazov,char k){
+    if(Root != NULL){
+		if(Root->type == typ){
+			nazov = Root->name;
+			return TRUE;
+		}else{
+			if(Root->id > k){
+				return symTableSearch(Root->lptr,typ,nazov,k);
+			}else{
+				return symTableSearch(Root->rptr,typ,nazov,k);
+			}
+		}
+	}else{
+		return FALSE;
+	}
+}
+
+void symTableInsert(symtab_elem_t* Root, char k, string nazov, elem_type typ){
+    if(*Root != NULL){
+		if(*Root->id == k){
+			*Root->type = typ;
+			*Root->name = nazov;
+		}else if(*Root->id < k){
+			symTableInsert(&(*Root)->rptr,k,nazov,typ);
+		}else{
+			symTableInsert(&(*Root)->lptr,k,nazov,typ);
+		}
+	}else{
+		symtab_elem_t tmp = malloc(sizeof(struct symtab_elem_t));
+		if(tmp != NULL){
+			tmp->id = k;
+			tmp->lptr = NULL;
+			tmp->rptr = NULL;
+			tmp->name = nazov;
+			tmp->type = typ;
+			*Root = tmp;
+		}
+	}
+}
--- symtable.h
+++ symtable.h
@@ -15,8 +15,9 @@ typedef enum {
 typedef struct symtab_t symtab_t;
 
 typedef struct symtab_elem_t {
-    char *id;
-    type_t typ;
+    string name;
+    char id;
+    type_t type;
     struct symtab_elem_t *lptr;
     struct symtab_elem_t *rptr;
 } symtab_elem_t;
@@ -25,3 +26,6 @@ struct symtab_t{
     symtab_elem_t *elements[TABLE_SIZE];
     size_t size;
 };
+
+int symTableSearch(symtab_elem_t Root, elem_type typ, string nazov,char k);
+void symTableInsert(symtab_elem_t* Root, char k, string nazov, elem_type typ);
